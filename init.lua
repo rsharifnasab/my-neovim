be5cfc947c3441b2bd0d0d275fb9a235e56376ff
Roozbeh Sharifnasab <rsharifnasab@gmail.com>
2024-05-05 00:05:17 +0330

    move gitsigns config

--- init.lua
+++ init.lua
@@ -664,7 +664,7 @@ require('lazy').setup({
   require 'plugins.lint',
   -- require 'plugins.autopairs',
   require 'plugins.tree',
-  require 'plugins.gitsigns', -- adds gitsigns recommend keymaps
+  require 'plugins.gitsigns',
 }, {
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
