a0ae61f5f86326244042ed25b191b539f2d51f30
Roozbeh Sharifnasab <rsharifnasab@gmail.com>
2024-11-20 15:48:22 +0330

    remove hardtime and flash

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -56,31 +56,4 @@ return {
       --  Check out: https://github.com/echasnovski/mini.nvim
     end,
   },
-  {
-    'm4xshen/hardtime.nvim',
-    dependencies = { 'MunifTanjim/nui.nvim', 'nvim-lua/plenary.nvim' },
-    opts = {
-      max_time = 1000,
-      max_count = 5,
-      disable_mouse = false,
-      hint = true,
-      notification = true,
-      allow_different_key = true,
-      enabled = false,
-      restriction_mode = 'block', -- block or hint
-    },
-  },
-  {
-    'folke/flash.nvim',
-    event = 'VeryLazy',
-    opts = {},
-    -- stylua: ignore
-    keys = {
-    { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
-    { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
-    { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
-    { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
-    { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
-  },
-  },
 }
