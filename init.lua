ce10c56a514a033d6544b7a381c8890c795b382e
Roozbeh Sharifnasab <rsharifnasab@gmail.com>
2024-11-25 15:08:08 +0330

    fix(init): deprecated nvim exec

--- init.lua
+++ init.lua
@@ -155,7 +155,7 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 vim.api.nvim_create_autocmd({ 'BufReadPost' }, {
   pattern = { '*' },
   callback = function()
-    vim.api.nvim_exec('silent! normal! g`"zv', false)
+    vim.cmd 'silent! normal! g`"zv'
   end,
 })
 
