36351f80308c272fbb00db65182e634faee9406e
Roozbeh Sharifnasab <rsharifnasab@gmail.com>
2025-03-23 12:03:41 +0330

    fix: parrot nvim

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -5,15 +5,32 @@ return {
   config = function()
     require('parrot').setup {
       -- the prefix used for all commands
-      cmd_prefix = 'Prt',
+      cmd_prefix = 'Parrot',
+      toggle_target = 'tabnew',
 
       providers = {
-        openai = {
-          api_key = os.getenv 'OPENAI_API_KEY',
-          endpoint = 'https://openrouter.ai/api/v1',
+        custom = {
+          style = 'openai',
+          api_key = os.getenv 'OPENROUTER_API_KEY',
+          endpoint = 'https://openrouter.ai/api/v1/chat/completions',
+          models = {
+            'openai/gpt-4o-mini',
+            'openai/gpt-4o',
+            'anthropic/claude-3.5-haiku',
+            'anthropic/claude-3.5-sonnet',
+            'anthropic/claude-3.7-sonnet',
+            'openai/o3-mini',
+          },
+          -- parameters to summarize chat
           topic = {
-            model = 'deepseek/deepseek-chat:',
-            params = { max_tokens = 32 },
+            model = 'openai/gpt-4o-mini',
+
+            params = { max_completion_tokens = 64 },
+          },
+          -- default parameters
+          params = {
+            chat = { temperature = 1.1, top_p = 1 },
+            command = { temperature = 1.1, top_p = 1 },
           },
         },
       },
