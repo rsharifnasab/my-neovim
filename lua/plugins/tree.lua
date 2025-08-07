d11348bf84df45e10fc8bf832d53aefc0fe3a16d
Roozbeh Sharifnasab <rsharifnasab@gmail.com>
2025-08-07 22:53:04 +0330

    feat(tree): add oil

--- lua/plugins/tree.lua
+++ lua/plugins/tree.lua
@@ -2,46 +2,70 @@
 -- https://github.com/nvim-neo-tree/neo-tree.nvim
 
 return {
-  'nvim-neo-tree/neo-tree.nvim',
-  version = '*',
-  dependencies = {
-    'nvim-lua/plenary.nvim',
-    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
-    'MunifTanjim/nui.nvim',
+  {
+    'stevearc/oil.nvim',
+    ---@module 'oil'
+    ---@type oil.SetupOpts
+    opts = {
+      default_file_explorer = false,
+      columns = {
+        'icon',
+        'permissions',
+        'size',
+        'mtime',
+      },
+      delete_to_trash = true,
+      watch_for_changes = true,
+      show_hidden = true,
+    },
+    -- Optional dependencies
+    dependencies = { { 'echasnovski/mini.icons', opts = {} } },
+    -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
+    -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
+    lazy = false,
   },
-  cmd = 'Neotree',
-  lazy = false,
-  keys = {
-    { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
-    { '<C-f>', ':Neotree toggle<CR>', { desc = 'NeoTree toggle' }, silent = true },
-  },
-  opts = {
-    close_if_last_window = false, -- Close Neo-tree if it is the last window left in the tab
-    popup_border_style = 'rounded',
-    enable_git_status = true,
-    enable_diagnostics = true,
-    open_files_do_not_replace_types = { 'terminal', 'trouble', 'qf' }, -- do not use windows containing these filetypes
-    filesystem = {
-      filtered_items = {
-        visible = true,
-        show_hidden_count = true,
-        hide_dotfiles = false,
-        hide_gitignored = false,
-        hide_by_name = { -- why people use macOS?
-          '.git',
-          '.DS_Store',
-          'thumbs.db',
+  {
+    'nvim-neo-tree/neo-tree.nvim',
+    version = '*',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+      'MunifTanjim/nui.nvim',
+    },
+    cmd = 'Neotree',
+    lazy = false,
+    keys = {
+      { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
+      { '<C-f>', ':Neotree toggle<CR>', { desc = 'NeoTree toggle' }, silent = true },
+    },
+    opts = {
+      close_if_last_window = false, -- Close Neo-tree if it is the last window left in the tab
+      popup_border_style = 'rounded',
+      enable_git_status = true,
+      enable_diagnostics = true,
+      open_files_do_not_replace_types = { 'terminal', 'trouble', 'qf' }, -- do not use windows containing these filetypes
+      filesystem = {
+        filtered_items = {
+          visible = true,
+          show_hidden_count = true,
+          hide_dotfiles = false,
+          hide_gitignored = false,
+          hide_by_name = { -- why people use macOS?
+            '.git',
+            '.DS_Store',
+            'thumbs.db',
+          },
         },
-      },
-      hijack_netrw_behavior = 'open_current',
-      follow_current_file = {
-        enabled = true,
-        leave_dirs_open = false,
-      },
-      window = {
-        mappings = {
-          ['\\'] = 'close_window',
-          ['<C-f>'] = 'close_window',
+        hijack_netrw_behavior = 'open_current',
+        follow_current_file = {
+          enabled = true,
+          leave_dirs_open = false,
+        },
+        window = {
+          mappings = {
+            ['\\'] = 'close_window',
+            ['<C-f>'] = 'close_window',
+          },
         },
       },
     },
