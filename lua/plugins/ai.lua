23c768822a7c7ceadf0733fdd996c832e60205aa
Roozbeh Sharifnasab <rsharifnasab@gmail.com>
2026-09-20 17:14:48 +0330

    feat: disable ai plugin cursor

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -1,9 +1,1 @@
-return {
-  {
-    'xTacobaco/cursor-agent.nvim',
-    config = function()
-      -- Toggle the agent terminal
-      vim.keymap.set('n', '<leader>ai', ':CursorAgent<CR>', { desc = 'Cursor Agent: Toggle terminal' })
-    end,
-  },
-}
+return {}
